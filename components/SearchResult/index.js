b8668739cdb70b02f5095d0d99126d08af51416a
Mrityunjay <mrityun.jay@indegene.com>
2021-05-25 19:34:31 +0530

    SearchResult component re-factor

--- components/SearchResult/index.js
+++ components/SearchResult/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import {ScrollView, useColorScheme, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  ScrollView,
+  useColorScheme,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const SearchResult = ({searchText}) => {
+const SearchResult = ({coinList, searchText}) => {
   const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+  const [filteredCoins, setFilteredCoins] = useState([]);
+
+  useEffect(() => {
+    if (searchText) {
+      const list = [];
+      for (let x = 0; x < coinList.length; x++) {
+        if (coinList[x].includes(searchText)) {
+          list.push(coinList[x]);
+        }
+      }
+      setFilteredCoins(list);
+    } else {
+      setFilteredCoins([]);
+    }
+  }, [searchText]);
 
   return (
     <ScrollView
@@ -17,14 +41,39 @@ const SearchResult = ({searchText}) => {
         borderTopRightRadius: 10,
       }}>
       <View>
-        {searchText.split('').map((text, index) => (
-          <Text key={index} style={{color: 'white'}}>
-            {text}
-          </Text>
+        {filteredCoins.map((coin, index) => (
+          <TouchableHighlight
+            onPress={() => console.log('pressed')}
+            key={coin}
+            style={{backgroundColor: 'rgba(222,145,220,0.1)'}}>
+            <View style={styles.coinNameContainer}>
+              <Text style={styles.coin}>{coin}</Text>
+              <Icon
+                name="arrow-left"
+                size={20}
+                color="#878f99"
+                style={{marginRight: 10, transform: [{rotate: '45deg'}]}}
+              />
+            </View>
+          </TouchableHighlight>
         ))}
       </View>
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  coinNameContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    minHeight: 50,
+    marginLeft: 10,
+  },
+  coin: {
+    color: 'white',
+    textTransform: 'uppercase',
+  },
+});
 
 export default SearchResult;
